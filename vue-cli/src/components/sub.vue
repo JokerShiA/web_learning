25ca93e93a0af471000d1fab19e8b60d78bc5919
石冬明 <sdm@ciqtek.com>
2019-08-15 09:53:05 +0800

    *emit

---
+++ vue-cli/src/components/sub.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="blog-post">
+    <h3>{{ post.title }}</h3>
+    <button v-on:click="$emit('enlarge-text',0.1)">Enlarge text</button>
+    <button v-on:click="$emit('narrow-text',0.1)">narrow text</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "button-counter",
+  props: ["post"],
+  data(){
+    return {
+
+    }
+  }
+};
+</script>
+
+<style>
+</style>
--- vue-cli/src/components/test.vue
+++ vue-cli/src/components/test.vue
@@ -1,32 +1,66 @@
 <template>
   <div class="test">
     <h1>{{ msg }}</h1>
-    <!-- <button v-on:click="reverseMessage">反转消息</button> -->
-    <p>Original message: "{{ msg }}"</p>
-    <p>Computed reversed message: "{{ reversedMessage }}"</p>
+    <button v-on:click="reverseMessage">反转消息</button>
+    <ul>
+      <li v-for="n in evenNumbers" :key="n">{{ n }}</li>
+    </ul>
+    <input v-on:keyup.enter="onEnterUp" v-on:keydown.enter="onEnterDown" />
+    <br />
+
+    <div id="blog-posts-events-demo">
+      <div :style="{ fontSize: postFontSize + 'em' }">
+        <sub-post
+          v-for="post in posts"
+          v-bind:key="post.id"
+          v-bind:post="post"
+          v-on:enlarge-text="postFontSize += $event"
+          v-on:narrow-text="postFontSize -= $event"
+        ></sub-post>
+      </div>
+    </div>
+
   </div>
 </template>
 
 <script>
+import subPost from "./sub.vue";
+
 export default {
   name: "test",
+  components: {
+    subPost
+  },
   props: {
     msg: String
   },
-  data () {
+  data() {
     return {
+      numbers: [1, 2, 3, 4, 5],
+      counter: 0,
+      posts:[{title: 'aa', id:1},{title: 'bb', id:2},{title: 'cc',id:3}],
+      postFontSize: 1
+    };
+  },
+  methods: {
+    reverseMessage: function() {
+      this.msg = this.msg
+        .split("")
+        .reverse()
+        .join("");
+    },
+    onEnterUp: function() {
+      console.log("enter up");
+    },
+    onEnterDown: function() {
+      console.log("enter down");
     }
   },
-  // methods: {
-  //   reverseMessage: function () {
-  //     this.msg = this.msg.split('').reverse().join('')
-  //   }
-  // }
   computed: {
-    // 计算属性的 getter
-    reversedMessage: function () {
-      // `this` 指向 vm 实例
-      return this.msg.split('').reverse().join('')
+    evenNumbers: function() {
+      return this.numbers.filter(function(number) {
+        return number % 2 === 0;
+      });
     }
   }
 };
